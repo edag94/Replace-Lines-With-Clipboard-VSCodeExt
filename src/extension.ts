2fc195e9db6358cd155f54e1ddab7db6c43ff5f5
Erick Dagenais <erdagena@microsoft.com>
2019-04-21 13:24:47 -0700

    done

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,18 @@
 import * as vscode from 'vscode';
-import replaceLineWIthClipboard from './replaceLine'
+import replaceLineWithClipboard from './replaceLine';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "replace-line-with-clipboard" is now active!');
-
-	let ext = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Replacing Line with clipboard!');
+	const ext : vscode.Disposable = vscode.commands.registerCommand('extension.ReplaceLineWithClipboard', () => {
 		if (vscode.window.activeTextEditor !== undefined){
-			const textEditor = vscode.window.activeTextEditor;
-			replaceLineWIthClipboard(textEditor);
+			const activeTextEditor = vscode.window.activeTextEditor;
+			replaceLineWithClipboard(activeTextEditor);
 		} 
 	});
 
 	context.subscriptions.push(ext);
 }
 
-export function deactivate() {}
+export function deactivate() {
+	console.log(':(');
+}
--- src/replaceLine.ts
+++ src/replaceLine.ts
@@ -1,31 +1,20 @@
 import * as vscode from 'vscode';
-import { Disposable, TextEditor, TextDocument, Selection, TextEditorEdit, TextLine, Range, Position, Clipboard } from "vscode";
+import { TextDocument, Selection, TextLine, Range, } from "vscode";
 
-
-export default (editor: vscode.TextEditor) : void => {
-	let selections: Selection[] = editor.selections;
+export default (editor: vscode.TextEditor) => {
+    let selections: Selection[] = editor.selections;
     var doc: TextDocument = editor.document;
-    
-    editor.edit((edit: TextEditorEdit): void => {
-        selections.forEach((selection: Selection, index: number) => {
-            for (let i = selection.start.line; i <= selection.end.line; i++) {
-                let selLine: TextLine = doc.lineAt(i);
-                let lineRange: Range = selLine.range;
-                let lineText: string = selLine.text;
-                vscode.env.clipboard.readText()
-                    .then(clipboard => {
-                        if (clipboard === undefined || clipboard === null) {
-                            vscode.window.showInformationMessage('Nothing currently saved to clipboard!');
-                        }
-                        else
-                            try{
-                                edit.replace(lineRange, clipboard)
-                            }
-                            catch (e){
-                                console.log(e);
-                            }
-                    });
-            }
-        });
-    })
+
+    vscode.env.clipboard.readText()
+        .then( (clipboard : string) => {
+            editor.edit(editBuilder => {
+                for (const cursorSelection of selections){
+                    const lineNumber : number = cursorSelection.active.line;
+                    const line : TextLine = doc.lineAt(lineNumber);
+                    const lineRange : Range = line.range;
+                    
+                    editBuilder.replace(lineRange, clipboard)
+                }
+            });
+        })
 }
